4b71b5308bc20eba6be7c14cf9ed758b48b69e2f
abhishek <abhishek.satapathy01@gmail.com>
2019-03-16 04:47:59 +0530

    Added graphs and routes for the same

--- app.py
+++ app.py
@@ -10,5 +10,24 @@ app = Flask(__name__)
 # =========
 # Start App
 # =========
-app.debug = True
-app.run(port='3000', debug=True)
+
+
+
+
+
+
+# =========
+# Graph
+# =========
+@app.route('/graph')
+@app.route('/index')
+def index(chartID = 'chart_ID', chart_type = 'bar', chart_height = 350):
+	chart = {"renderTo": chartID, "type": chart_type, "height": chart_height,}
+	series = [{"name": 'Label1', "data": [1,2,3]}, {"name": 'Label2', "data": [4, 5, 6]}]
+	title = {"text": 'My Title'}
+	xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
+	yAxis = {"title": {"text": 'yAxis Label'}}
+	return render_template('graph.html', chartID=chartID, chart=chart, series=series, title=title, xAxis=xAxis, yAxis=yAxis)
+
+if __name__ == "__main__":
+	app.run(debug = True, host='0.0.0.0', port=8080, passthrough_errors=True)
