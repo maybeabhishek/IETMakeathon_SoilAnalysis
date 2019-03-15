4737fd9b70141a36afa971a0b964ffce9c01ee51
Shreyansh <erixerns@gmail.com>
2019-03-15 20:29:16 +0530

    Added navbar

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 
 app = Flask(__name__)
 
+
+@app.route("/")
+def renderRoot():
+  return render_template("index.html")
 
 
 
